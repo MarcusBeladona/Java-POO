ad4ad2f0e15562cedd06c11811ac2e6899e47ae6
Mark Tremblay <mv3265@gmail.com>
2018-09-20 11:15:28 -0300

    Herança Adicionada.

---
+++ Herança/exec/Principal.java
@@ -0,0 +1,11 @@
+package Herança.exec;
+import Herança.model.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Automovel auto = new Automovel(50,6,"azul",2,"POC69");
+        Automovel novo = new Automovel();
+    }
+}
---
+++ Herança/model/Transporte.java
@@ -0,0 +1,20 @@
+package Herança.model;
+
+public class Transporte {
+
+    private int capacidade;
+
+    public Transporte(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public Transporte () { }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+}
